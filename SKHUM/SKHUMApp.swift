484e9027489f5891bda48de56a40ac7c8a017e14
judongseok <wnehdtjr5@naver.com>
2021-04-10 03:38:23 +0900

    Initial Commit

---
+++ SKHUM/SKHUMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKHUMApp.swift
+//  SKHUM
+//
+//  Created by judongseok on 2021/04/10.
+//
+
+import SwiftUI
+
+@main
+struct SKHUMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
