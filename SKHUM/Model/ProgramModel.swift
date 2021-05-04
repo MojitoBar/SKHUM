fd3e700c69eadfd27081566be3fc1be26a998138
MojitoBar <wnehdtjr5@gmail.com>
2021-05-04 23:24:22 +0900

    Change MainView's NavBar Design

--- SKHUM/Model/ProgramModel.swift
+++ SKHUM/Model/ProgramModel.swift
@@ -7,14 +7,6 @@
 
 import Foundation
 
-//struct Program: Identifiable, Decodable {
-//    var id: Int
-//    var endDate: Date?
-//    var givingMileage: Int
-//    var programName: String
-//    var startDate: Date?
-//}
-
 struct Program: Identifiable, Decodable {
     var id: Int
     var name: String
